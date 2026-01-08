f9be7b2fa70ff9a82a1a8e310d43e2561f019c4d
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-01-08 19:00:39 +0900

    #8 [FEAT] 로그인 페이지

--- src/components/onboarding/LoginButton.tsx
+++ src/components/onboarding/LoginButton.tsx
@@ -1,6 +1,7 @@
+import { useNavigate } from 'react-router-dom';
+
 type LoginButtonProps = {
   platform: 'google' | 'naver' | 'kakao';
-  onClick?: () => void;
 };
 
 const platformStyles: Record<string, string> = {
@@ -15,11 +16,18 @@ const platformNames: Record<string, string> = {
   kakao: '카카오톡으',
 };
 
-const LoginButton = ({ platform, onClick }: LoginButtonProps) => {
+const LoginButton = ({ platform }: LoginButtonProps) => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // TODO: 실제 로그인 로직 구현
+    navigate('/onboarding');
+  };
+
   return (
     <button
-      className={`flex w-full items-center justify-center gap-2 rounded-lg py-3 ${platformStyles[platform]}`}
-      onClick={onClick}
+      className={`flex w-full cursor-pointer items-center justify-center gap-2 rounded-lg py-3 ${platformStyles[platform]}`}
+      onClick={handleLogin}
     >
       <img src={`/icons/login/${platform}.svg`} alt={`${platform} Logo`} className="h-6 w-6" />
       <span className="text-body-16B">{`${platformNames[platform]}로 시작하기`}</span>
