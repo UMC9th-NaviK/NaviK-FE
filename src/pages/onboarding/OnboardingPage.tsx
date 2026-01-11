04529118d77ca5a3ea58dad656033b114fae354f
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-01-11 23:52:09 +0900

    #8 [CHORE] 닉네임 선택 처리

--- src/pages/onboarding/OnboardingPage.tsx
+++ src/pages/onboarding/OnboardingPage.tsx
@@ -5,6 +5,8 @@ import BackHeader from './BackHeader';
 
 const OnboardingPage = () => {
   const [career, setCareer] = useState<'신입' | '경력'>('신입');
+  const [name, setName] = useState('');
+  const [nickname, setNickname] = useState('');
 
   return (
     <div className="relative flex h-dvh flex-col overflow-hidden">
@@ -18,11 +20,24 @@ const OnboardingPage = () => {
         <BackHeader title="프로필 정보 입력" />
         <div className="mt-8.25 flex flex-col gap-7.5">
           <div className="flex flex-col gap-6">
-            <Input title="이름" placeholder="이름을 입력해주세요" />
-            <Input title="닉네임" placeholder="닉네임을 입력해주세요" />
+            <Input
+              title="이름"
+              placeholder="이름을 입력해주세요"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Input
+              title="닉네임"
+              placeholder="닉네임을 입력해주세요 (선택)"
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+            />
             <Career value={career} onChange={setCareer} />
           </div>
-          <button className="bg-primary-blue-500 text-base-100 text-body-16B w-full cursor-pointer rounded-lg py-3 text-center">
+          <button
+            className={`text-base-100 text-body-16B w-full rounded-lg py-3 text-center transition-all ${name ? 'bg-primary-blue-500 cursor-pointer opacity-100' : 'bg-primary-blue-200 cursor-not-allowed opacity-50'}`}
+            disabled={!name}
+          >
             다음
           </button>
         </div>
