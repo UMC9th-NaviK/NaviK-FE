41501950eafc169bc89a617647f391b474cfed44
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-01-13 02:30:38 +0900

    #8 [FEAT] 버튼 disabled 요소 추가

--- src/components/common/ButtonRound.tsx
+++ src/components/common/ButtonRound.tsx
@@ -1,13 +1,15 @@
 type ButtonProps = {
   onClick: () => void;
   text: string;
+  disabled?: boolean;
 };
 
-const ButtonRound = ({ onClick, text }: ButtonProps) => {
+const ButtonRound = ({ onClick, text, disabled }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="bg-primary-blue-500 text-body-16B text-base-100 mx-4 w-full cursor-pointer rounded-full py-3"
+      disabled={disabled}
+      className={`${disabled ? 'cursor-default opacity-50' : 'cursor-pointer'} bg-primary-blue-500 text-body-16B text-base-100 w-full rounded-full py-3`}
     >
       {text}
     </button>
