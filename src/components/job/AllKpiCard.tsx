0cf43a0f28105934bbac1c10f19d14f1fd7902cd
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-01-14 16:38:52 +0900

    #8 [FEAT] 전체 KPI 카드 페이지

--- src/components/job/AllKpiCard.tsx
+++ src/components/job/AllKpiCard.tsx
@@ -1,5 +1,23 @@
 const AllKpiCard = () => {
-  return <div>AllKpiCard</div>;
+  return (
+    <div className="flex flex-col gap-4 pb-7">
+      {/* 전체 카드 리스트 */}
+      <div className="grid grid-cols-2 gap-4">
+        {/* 카드 20개 반복 */}
+        {[...Array(20)].map((_, index) => (
+          <img
+            key={index}
+            src="/images/kpi/example.png"
+            className="aspect-2/3 w-full object-cover"
+          />
+        ))}
+      </div>
+      <div className="bg-base-200 text-base-500 text-caption-12M flex items-center gap-1 rounded-lg px-4 py-3">
+        <img src="/icons/warning.svg" className="h-3.5 w-3.5" />
+        <p>AI 검사 결과 유의사항</p>
+      </div>
+    </div>
+  );
 };
 
 export default AllKpiCard;
