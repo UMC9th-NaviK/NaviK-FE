9d27fda42ba7a2512fa34d029174120c29e07cc1
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-01-15 14:08:46 +0900

    #8 [FEAT] 극복 역량 페이지

--- src/components/job/OvercomePower.tsx
+++ src/components/job/OvercomePower.tsx
@@ -1,12 +1,20 @@
 import CardSlider from '../common/CardSlider';
+import StudySuggest from './StudySuggest';
 
 const OvercomePower = () => {
   return (
     <div className="flex flex-col gap-8">
       <CardSlider title="극복 역량" />
-      <div>
-        <p className="text-heading-18B mb-4">이렇게 극복하는건 어때요</p>
-        {/* 내용 */}
+      <div className="mb-6.75 flex flex-col gap-4 p-4">
+        <span className="flex items-center gap-2">
+          <img src="/icons/jobs/mouse-cursor.svg" className="h-6 w-6" />
+          <p className="text-heading-20B text-base-900 flex">
+            이렇게&nbsp;
+            <p className="text-primary-blue-500">극복</p>
+            하는건 어때요?
+          </p>
+        </span>
+        <StudySuggest />
       </div>
     </div>
   );
