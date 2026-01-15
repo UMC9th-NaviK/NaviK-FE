a4383aad164a8dc3c5835a1b7370f1558940a0fc
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-01-15 14:08:35 +0900

    #8 [FEAT] 핵심 역량 페이지

--- src/components/job/CorePower.tsx
+++ src/components/job/CorePower.tsx
@@ -1,12 +1,20 @@
 import CardSlider from '../common/CardSlider';
+import JobSuggest from './JobSuggest';
 
 const CorePower = () => {
   return (
     <div className="flex flex-col gap-8">
       <CardSlider title="핵심 역량" />
-      <div>
-        <p className="text-heading-18B mb-4">이렇게 활용하면 좋아요</p>
-        {/* 내용 */}
+      <div className="mb-6.75 flex flex-col gap-4 p-4">
+        <span className="flex items-center gap-2">
+          <img src="/icons/jobs/circle-rounded.svg" className="h-6 w-6" />
+          <p className="text-heading-20B text-base-900 flex">
+            이렇게&nbsp;
+            <p className="text-primary-blue-500">활용</p>
+            하면 좋아요!
+          </p>
+        </span>
+        <JobSuggest />
       </div>
     </div>
   );
