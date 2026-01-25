060f6f04e1af550cda64266f0e550fe3d033a487
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-01-25 21:34:28 +0900

    #13 [FEAT] 직무요약 컴포넌트

---
+++ src/components/setup/JobSummary.tsx
@@ -0,0 +1,10 @@
+const JobSummary = () => {
+  return (
+    <div className="shadow-card bg-base-100 flex flex-col gap-1 rounded-2xl p-4">
+      <p className="text-opacity-black-80 text-heading-20B">직무 요약</p>
+      <p className="text-opacity-black-40 text-body-14M">본문 내용</p>
+    </div>
+  );
+};
+
+export default JobSummary;
