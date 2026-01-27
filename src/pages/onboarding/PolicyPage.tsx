6f6d58389b3f516cd63f41e1aff943fbca79629e
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-01-27 18:53:48 +0900

    #13 [FEAT] 전체동의 아이템 분리

--- src/pages/onboarding/PolicyPage.tsx
+++ src/pages/onboarding/PolicyPage.tsx
@@ -1,8 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 import BackHeader from '../../components/common/BackHeader';
 import ButtonSquare from '../../components/common/ButtonSquare';
 import PolicyItem from '../../components/onboarding/PolicyItem';
+import PolicyAllAgree from '../../components/onboarding/PolicyAllAgree';
+import { usePolicyAgreement } from '../../hooks/usePolicyAgreement';
+import { POLICIES } from '../../constants/policy';
 
 const PolicyPage = () => {
+  const navigate = useNavigate();
+  const { agreements, allChecked, handleToggleAll, handleToggleItem } = usePolicyAgreement();
+
+  const handleViewPolicy = (policyId: string) => {
+    navigate(`/onboarding?step=1&detail=${policyId}`);
+  };
+
+  const handleNext = () => {
+    navigate('/onboarding?step=2');
+  };
+
   return (
     <>
       <BackHeader title="이용 약관" />
@@ -12,21 +27,25 @@ const PolicyPage = () => {
           <br />
           약관 동의가 필요해요
         </p>
-        <div className="flex w-full flex-col px-4">
+        <div className="flex w-full flex-col px-4 pb-24">
           <div className="flex flex-col gap-4">
-            <div className="border-b-base-200 flex gap-2 border-b py-4">
-              {/* 체크박스 */}
-              <p className="text-heading-18B text-primary-blue-500">전체 동의하기</p>
-            </div>
-            <div>
-              <PolicyItem />
-              <PolicyItem />
-              <PolicyItem />
-              <PolicyItem />
+            <PolicyAllAgree checked={allChecked} onToggle={handleToggleAll} />
+
+            <div className="flex flex-col">
+              {POLICIES.map((policy) => (
+                <PolicyItem
+                  key={policy.id}
+                  id={policy.id}
+                  title={policy.title}
+                  checked={agreements[policy.id as keyof typeof agreements]}
+                  onToggle={handleToggleItem}
+                  onView={handleViewPolicy}
+                />
+              ))}
             </div>
           </div>
           <div className="absolute right-0 bottom-0 left-0 z-20 flex justify-center px-4 pb-6">
-            <ButtonSquare onClick={() => {}} text="나의 직무 선택하기" />
+            <ButtonSquare onClick={handleNext} text="나의 직무 선택하기" disabled={!allChecked} />
           </div>
         </div>
       </div>
