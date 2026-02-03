0c11503c7c9e4ef250113626b75164d88d5b10b7
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-02-04 00:55:53 +0900

    #35 [FEAT] 홈 헤더 적용

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,5 @@
 import { HomeBackground } from '../../components/home/HomeBackground';
+import HomeHeader from '../../components/home/HomeHeader';
 
 const HomePage = () => {
   return (
@@ -6,7 +7,7 @@ const HomePage = () => {
       <div className="relative min-h-dvh overflow-hidden">
         <HomeBackground />
         <div className="relative z-10 flex flex-col gap-4">
-          <div>header</div>
+          <HomeHeader />
           <div className="flex flex-col gap-8 px-4">
             <p className="text-heading-20B text-base-100">
               김나비님
