a38dc305c48a7802e45cc18285e618b89f61e903
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-02-03 15:46:33 +0900

    #35 [FEAT] 헤더 인포 이미지

--- src/components/common/CardHeader.tsx
+++ src/components/common/CardHeader.tsx
@@ -1,14 +1,23 @@
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const CardHeader = () => {
-  const navigate = useNavigate();
+  const [clickInfo, setClickInfo] = useState(false);
+
+  const onInfoClick = () => {
+    setClickInfo(!clickInfo);
+  };
 
   return (
     <div className="relative flex w-full items-center justify-between px-6 pt-6 pb-4">
       <p className="text-heading-20B text-base-900">KPI 진단 결과</p>
-      <button>
+      <button onClick={onInfoClick}>
         <img src="/icons/reports/info.svg" className="h-6 w-6" />
       </button>
+      {clickInfo && (
+        <div className="absolute top-8 -right-2 z-10 w-60 overflow-hidden">
+          <img src="/images/infoText.png" />
+        </div>
+      )}
     </div>
   );
 };
