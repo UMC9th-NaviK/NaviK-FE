ca64b620dfcb25d7c83c0c8a57322fa3218dfb4f
qkrdmsthff <126754457+qkrdmsthff@users.noreply.github.com>
2026-02-05 22:46:44 +0900

    [FEAT] 카드 슬라이더 수정

--- src/pages/report/OvercomingKPIPage.tsx
+++ src/pages/report/OvercomingKPIPage.tsx
@@ -22,14 +22,17 @@ const OvercomingKPIPage = () => {
                     </div>
                     <div className='flex flex-col rounded-[8px] p-[16px] gap-[16px] bg-white shadow-[0_0_10px_0_#DBEBFE]'>
                         <p className='text-heading-18B text-[#1B1B1B]'> <span className='text-[#4E83F9]'> 김나비</span>님을 위한 추천 스터디 📚 </p>
-                        <div className="overflow-x-auto">
+                        <div className="overflow-x-auto scrollbar-hide pr-5 snap-x snap-mandatory scroll-pl-[22px]">
                             <div className='flex flex-nowrap w-max box-border gap-[16px] scroll-smooth'>
                                 <RecommendationNotice role={'pm'} />
                                 <RecommendationNotice role={'pm'} />
                                 <RecommendationNotice role={'pm'} />
                                 <RecommendationNotice role={'pm'} />
+
+                                <div className="flex-shrink-0 w-[1px] h-full" />
                             </div>
                         </div>
+                        
                         <div className='flex flex-1 justify-end gap-[4px]'> 
                             <p className='text-caption-12M text-[#11111199]'> 전체 스터디 보러가기 </p>
                             <img 
