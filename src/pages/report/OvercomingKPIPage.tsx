2323aa76f18d7f28510ee90a0853f92694bec3f3
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-02-06 18:53:54 +0900

    #35 [CHORE] 카드 슬라이더 수정 적용

--- src/pages/report/OvercomingKPIPage.tsx
+++ src/pages/report/OvercomingKPIPage.tsx
@@ -1,52 +1,55 @@
-import KPILocalNavbar from '../../components/report/KPILocalNavbar'
-import ReportNavbar from '../../components/report/ReportNavbar'
-import CardSlider from '../../components/common/CardSlider'
-import RecommendationNotice from '../../components/report/RecommendationNotice'
-import { FOOTERPB } from '../../components/common/Footer'
+import KPILocalNavbar from '../../components/report/KPILocalNavbar';
+import ReportNavbar from '../../components/report/ReportNavbar';
+import CardSlider from '../../components/common/CardSlider';
+import RecommendationNotice from '../../components/report/RecommendationNotice';
+import { FOOTERPB } from '../../components/common/Footer';
 
 const OvercomingKPIPage = () => {
-    return (
-        <div className={`${FOOTERPB}`}>
-            <ReportNavbar />
-            <KPILocalNavbar />
+  return (
+    <div className={`${FOOTERPB}`}>
+      <ReportNavbar />
+      <KPILocalNavbar />
 
-            <div className='flex flex-col bg-white-background gap-[32px] pb-[16px]'>
-                <CardSlider title={'극복 역량'} />
+      <div className="bg-white-background flex flex-col gap-[32px] pb-[16px]">
+        <CardSlider />
 
-                <div className='flex flex-col px-[16px] gap-[16px]'>
-                    <div className='flex flex-1 gap-[8px]'>
-                        <img 
-                        src="/icons/reports/Vector.svg"
-                        alt="vector"
-                        />
-                        <p className='text-heading-20B'> 이렇게 <span className='text-[#4E83F9]'>극복</span>하는 건 어때요? </p>
-                    </div>
-                    <div className='flex flex-col rounded-[8px] p-[16px] gap-[16px] bg-white shadow-[0_0_10px_0_#DBEBFE]'>
-                        <p className='text-heading-18B text-[#1B1B1B]'> <span className='text-[#4E83F9]'> 김나비</span>님을 위한 추천 스터디 📚 </p>
-                        <div className="overflow-x-auto scrollbar-hide pr-5 snap-x snap-mandatory scroll-pl-[22px]">
-                            <div className='flex flex-nowrap w-max box-border gap-[16px] scroll-smooth'>
-                                <RecommendationNotice role={'pm'} />
-                                <RecommendationNotice role={'pm'} />
-                                <RecommendationNotice role={'pm'} />
-                                <RecommendationNotice role={'pm'} />
+        <div className="flex flex-col gap-[16px] px-[16px]">
+          <div className="flex flex-1 gap-[8px]">
+            <img src="/icons/reports/Vector.svg" alt="vector" />
+            <p className="text-heading-20B">
+              {' '}
+              이렇게 <span className="text-[#4E83F9]">극복</span>하는 건 어때요?{' '}
+            </p>
+          </div>
+          <div className="flex flex-col gap-[16px] rounded-[8px] bg-white p-[16px] shadow-[0_0_10px_0_#DBEBFE]">
+            <p className="text-heading-18B text-[#1B1B1B]">
+              {' '}
+              <span className="text-[#4E83F9]"> 김나비</span>님을 위한 추천 스터디 📚{' '}
+            </p>
+            <div className="scrollbar-hide snap-x snap-mandatory scroll-pl-[22px] overflow-x-auto pr-5">
+              <div className="box-border flex w-max flex-nowrap gap-[16px] scroll-smooth">
+                <RecommendationNotice role={'pm'} />
+                <RecommendationNotice role={'pm'} />
+                <RecommendationNotice role={'pm'} />
+                <RecommendationNotice role={'pm'} />
 
-                                <div className="flex-shrink-0 w-[1px] h-full" />
-                            </div>
-                        </div>
-                        
-                        <div className='flex flex-1 justify-end gap-[4px]'> 
-                            <p className='text-caption-12M text-[#11111199]'> 전체 스터디 보러가기 </p>
-                            <img 
-                            src="/icons/reports/material-symbols_arrow-back-ios-new-rounded.svg"
-                            alt=""
-                            className='w-[16px] h-[16px]'
-                            />
-                        </div>
-                    </div>
-                </div>
+                <div className="h-full w-[1px] flex-shrink-0" />
+              </div>
             </div>
-        </div>
-    )
-}
 
-export default OvercomingKPIPage
+            <div className="flex flex-1 justify-end gap-[4px]">
+              <p className="text-caption-12M text-[#11111199]"> 전체 스터디 보러가기 </p>
+              <img
+                src="/icons/reports/material-symbols_arrow-back-ios-new-rounded.svg"
+                alt=""
+                className="h-[16px] w-[16px]"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OvercomingKPIPage;
