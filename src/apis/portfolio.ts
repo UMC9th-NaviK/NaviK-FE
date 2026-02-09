f170e856642447c772b18994c0a475d03816bd60
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-02-09 18:28:35 +0900

    #50 [CHORE] api 경로 슬래쉬 추가

--- src/apis/portfolio.ts
+++ src/apis/portfolio.ts
@@ -3,7 +3,7 @@ import type { RequestPortfolio, ResponsePortfolio } from '../types/portfolio';
 import axiosInstance from './axios';
 
 export async function postPortfolio(portfolio: RequestPortfolio): Promise<ResponsePortfolio> {
-  const { data } = await axiosInstance.post<CommonResponse<ResponsePortfolio>>(`portfolios`, {
+  const { data } = await axiosInstance.post<CommonResponse<ResponsePortfolio>>(`/portfolios`, {
     ...portfolio,
   });
 
