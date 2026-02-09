634a76ec37838879f9ad3ed29ca8f2730ab5f793
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-02-09 23:46:27 +0900

    #57 [FEAT] 포트폴리오 관련 api 추가

--- src/types/portfolio.ts
+++ src/types/portfolio.ts
@@ -1,10 +1,30 @@
 export type RequestPortfolio = {
   inputType: 'TEXT' | 'FILE';
-  content: string;
-  fileUrl: string;
+  content?: string;
+  fileUrl?: string;
 };
 
 export type ResponsePortfolio = {
   id: number;
   inputType: 'TEXT' | 'FILE';
+  status: 'COMPLETED' | 'RETRY_REQUIRED' | 'FAILED';
+};
+
+export type RequestPortfolioInfo = {
+  qB1: number;
+  qB2: number;
+  qB3: number;
+  qB4: number;
+  qB5: number;
+};
+
+export type PortfolioStatus = 'PENDING' | 'PROCESSING' | 'COMPLETED' | 'RETRY_REQUIRED' | 'FAILED';
+
+export type ResponsePortfolioStatus = {
+  portfolioId: number;
+  status: PortfolioStatus;
+};
+
+export type ResponseAdditionalInfo = {
+  portfolioId: number;
 };
