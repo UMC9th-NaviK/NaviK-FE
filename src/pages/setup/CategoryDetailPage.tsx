8c35a901306eadcfac6d83a755367f888481b5e8
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-02-10 13:55:22 +0900

    #57 [FEAT] 임시 로딩 페이지 추가

--- src/pages/setup/CategoryDetailPage.tsx
+++ src/pages/setup/CategoryDetailPage.tsx
@@ -2,6 +2,7 @@ import { useSearchParams, useParams } from 'react-router-dom';
 import CategoryIntro from '../../components/setup/CategoryIntro';
 import CategorySummary from '../../components/setup/CategorySummary';
 import SurveyStep from '../../components/setup/SurveyStep';
+import LoadingPolling from '../../components/setup/LoadingPolling';
 
 const CategoryDetailPage = () => {
   const [searchParams] = useSearchParams();
@@ -16,6 +17,7 @@ const CategoryDetailPage = () => {
     <div className="relative flex min-h-dvh flex-col bg-white">
       {step === '1' && <CategoryIntro categoryId={id} />}
       {step === '2' && <CategorySummary categoryId={id} />}
+      {step === 'loading' && <LoadingPolling />}
       {step === '3' && <SurveyStep categoryId={id} />}
     </div>
   );
