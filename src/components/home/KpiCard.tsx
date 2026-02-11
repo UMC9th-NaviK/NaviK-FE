e145e5274b0ce6c8c9d1b8da9fd37b02cf4f7be3
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-02-11 16:57:01 +0900

    #63 [FEAT] 홈 카드 이미지 추가

--- src/components/home/KpiCard.tsx
+++ src/components/home/KpiCard.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import { useUser } from '../../hooks/useUser';
 
 const KpiCard = () => {
+  const { job } = useUser();
   const [isFlipped, setIsFlipped] = useState(false);
 
   const handleClick = () => {
@@ -10,30 +12,30 @@ const KpiCard = () => {
   return (
     <div className="relative h-full">
       {/* 배경 이미지 */}
-      <img src="/images/home/card-bg-pm.png" className="absolute bottom-0 left-0" alt="" />
+      <img src={`/images/home/card-bg-${job}.png`} className="absolute bottom-0 left-0" alt="" />
 
       {/* 카드 영역 - 전체 클릭 가능 */}
       {/* TODO: 서서히 바뀌기 */}
       <button onClick={handleClick} className="absolute inset-0">
         <div className="relative flex h-full items-center justify-center">
           <div className="flex items-center">
-            <div className="z-0 -mr-8 w-25 shrink-0 -rotate-[8deg] transition-transform duration-300">
+            <div className="z-0 -mr-6 w-25 shrink-0 -rotate-[8deg] transition-transform duration-300">
               <img
-                src={`/images/kpi/pm-${isFlipped ? 'front' : 'back'}.png`}
+                src={`/images/kpi/${job}-${isFlipped ? 'front' : 'back'}.png`}
                 alt="KPI 카드 1"
                 className="w-full"
               />
             </div>
-            <div className="z-10 -mr-8 w-25 shrink-0 transition-transform duration-300">
+            <div className="z-10 -mr-6 w-25 shrink-0 transition-transform duration-300">
               <img
-                src={`/images/kpi/pm-${isFlipped ? 'front' : 'back'}.png`}
+                src={`/images/kpi/${job}-${isFlipped ? 'front' : 'back'}.png`}
                 alt="KPI 카드 2"
                 className="w-full"
               />
             </div>
             <div className="z-20 w-25 shrink-0 rotate-[8deg] transition-transform duration-300">
               <img
-                src={`/images/kpi/pm-${isFlipped ? 'front' : 'back'}.png`}
+                src={`/images/kpi/${job}-${isFlipped ? 'front' : 'back'}.png`}
                 alt="KPI 카드 3"
                 className="w-full"
               />
