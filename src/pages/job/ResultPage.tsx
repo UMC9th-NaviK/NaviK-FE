72acfc9bb65221a8c4c7ba86cf2445ae948d27fe
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-02-12 22:53:22 +0900

    hotfix: 토큰 재발급 로직 추가

--- src/pages/job/ResultPage.tsx
+++ src/pages/job/ResultPage.tsx
@@ -4,10 +4,26 @@ import AllKpiCard from '../../components/job/AllKpiCard';
 import OvercomePower from '../../components/job/OvercomePower';
 import CorePower from '../../components/job/CorePower';
 import CardHeader from '../../components/common/CardHeader';
+import { refreshAccessToken } from '../../apis/auth';
+import { useNavigate } from 'react-router-dom';
 
 const TABS = [{ component: AllKpiCard }, { component: CorePower }, { component: OvercomePower }];
 
 const ResultPage = () => {
+  const navigate = useNavigate();
+
+  useState(async () => {
+    try {
+      const { accessToken } = await refreshAccessToken();
+      localStorage.setItem('accessToken', accessToken);
+      console.log('✅ Token refreshed');
+    } catch (error) {
+      console.error('❌ Token refresh failed:', error);
+      alert('토큰 갱신에 실패했습니다. 다시 로그인해주세요.');
+      navigate('/login', { replace: true });
+      return;
+    }
+  });
   const [selected, setSelected] = useState(0);
 
   const CurrentComponent = TABS[selected].component;
