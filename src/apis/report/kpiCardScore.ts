40c1f64ae429f33d4de1eddae20a133ad1c19218
qkrdmsthff <126754457+qkrdmsthff@users.noreply.github.com>
2026-02-12 19:16:10 +0900

    [FEAT] API 정의

---
+++ src/apis/report/kpiCardScore.ts
@@ -0,0 +1,14 @@
+import type { KPICardBase, KPICardResponse, KPIcardScoreMonthlyResult, KPIcardScorePercentageResult } from "../../types/kpiCard";
+import axiosInstance from "../axios"
+
+export const getKPIScorePercentile = async (kpiCardId : number) : Promise<KPICardResponse<KPIcardScorePercentageResult>>=> {
+    const response = await axiosInstance.get(`/kpi-scores/${kpiCardId}/percentile`, {})
+
+    return response.data;
+}
+
+export const getKPIScoreMonthlyChangeRate = async () : Promise<KPICardResponse<KPIcardScoreMonthlyResult>> => {
+    const response = await axiosInstance.get(`/kpi-scores/monthly-change-rate`, {})
+
+    return response.data;
+}
